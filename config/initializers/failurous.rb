72cd84e5f43461a6c0bc17c04c1bcb0e139856ea
raimo-t <rtuisku@niksula.hut.fi>
2010-11-10 00:48:58 +0200

    Remove Failurous

--- config/initializers/failurous.rb
+++
@@ -1,6 +0,0 @@
-require 'failurous'
-
-Failurous::Config.server_address = 'http://failurous.r10.railsrumble.com/'
-Failurous::Config.api_key = '2de92b64-d7c3-4c9b-8290-016ba49ab64d'
-
-Rails.application.config.middleware.use(Failurous::FailMiddleware)
