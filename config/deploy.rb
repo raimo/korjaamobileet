032dac01acc43648c411db570f2990bf33067568
Raimo Tuisku <raimo.tuisku@gmail.com>
2011-10-26 00:36:32 +0300

    New configs

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,8 @@
-$:.unshift(File.expand_path('./lib', ENV['rvm_path'])) # Add RVM's lib directory to the load path.
+$:.unshift(File.expand_path('./lib', ENV['rvm_path']))
 require "rvm/capistrano"
 set :rvm_ruby_string, File.new('.rvmrc').read.split(' ').find{|s|s.include?('@')}
 set :rvm_type, :user
+
 
 set :application, "korjaamobileet"
 set :repository,  "git://github.com/raimo/korjaamobileet.git"
@@ -14,41 +15,33 @@ set :use_sudo, false
 set :deploy_to, "/home/users/raimo/#{application}"
 set :group_writable, false
 
-set :rails_env, "production"
-set :startcmd, "CATALINA_PID=tomcat.pid tomcat/bin/startup.sh;"
-set :stopcmd, "CATALINA_PID=tomcat.pid tomcat/bin/shutdown.sh -force;"
+set :pid, "#{current_path}/tmp/pids/passenger.31010.pid"
+set :bundle, "(cd #{current_path}; bundle)"
+set :cmd, "(cd #{current_path}; bundle exec passenger start -d -p 31010 -e production)"
 
 role :app, "kirsikka.kapsi.fi"
 role :web, "kirsikka.kapsi.fi"
 role :db,  "kirsikka.kapsi.fi", :primary => true
 
 namespace :deploy do
-
-  task :warble do
-    run "cd #{release_path};bundle exec warble RAILS_ENV=#{rails_env}"
-    run "rm -rf tomcat/webapps/leet.war tomcat/webapps/leet"
-    run "mv #{release_path}/leet.war tomcat/webapps"
-  end
-
-  task :start do
-    run startcmd
-  end
- 
   task :stop do
-    run stopcmd
+    run("test -s '#{pid}' && kill -QUIT `cat '#{pid}'`")
   end
 
   task :restart do
-    run stopcmd+startcmd
+    run("#{bundle} && touch #{current_path}/tmp/restart.txt")
   end
 
-  task :bundle do
-    run "cd #{release_path} && bundle"
+  task :start do
+    run("#{bundle} && (test -s '#{pid}' && kill -0 `cat '#{pid}'`) || #{cmd}")
   end
+end
 
+namespace :customs do
   task :config do
     run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
   end
 end
 
-after 'deploy:update_code', 'deploy:config', 'deploy:bundle', 'deploy:warble'
+after "deploy:update_code", "customs:config"
+
